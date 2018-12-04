e954d161b41f522a0415db6b0587420d2e0cf838
VanBantam <thetopquark@gmail.com>
2018-12-04 12:58:06 -0800

    I can inject my own javascript code into the app.

--- src/assets/js/test_data.js
+++ src/assets/js/test_data.js
@@ -1,3 +1,17 @@
+/*
+This script will be injected by gulp.  
+The Admin-UI-Development/inject.js script has the code for injecting scripts.
+// NOTE in this code block the double undrscores, __, were used to replace the 
+// double astrixs, **.  The reason for this is that the code wouldn't comment out.
+// Replace __ with ** to restore the original code. 
+// var injectScripts = gulp.src([
+    // path.join(conf.paths.src, '/assets/js/__/.js'),
+    // path.join(conf.paths.src, '/app/__/*.module.js'),
+    // path.join(conf.paths.src, '/app/__/*.js'),
+    // path.join('!' + conf.paths.src, '/app/__/*.spec.js'),
+    // path.join('!' + conf.paths.src, '/app/__/*.mock.js'),
+  // ])
+*/
 var hf = 'hello friend!';
 
 console.log(hf);
