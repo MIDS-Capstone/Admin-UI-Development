5888b99fc205e48fda9a23fde7a496e7a9e00d0d
VanBantam <thetopquark@gmail.com>
2018-12-05 08:27:49 -0800

    Testing rest API integration with pricer-poc.

--- src/app/app.js
+++ src/app/app.js
@@ -16,12 +16,3 @@ angular.module('BlurAdmin', [
   'BlurAdmin.theme',
   'BlurAdmin.pages'
 ]);
-
-// var App = angular.module('App', []);
-
-// App.controller('test_data', function($scope, $http)){
-//   $http.get('test_data.json')
-//        .then(function(res){
-//           $scope.test_data = res.data;
-//        });
-// };
